7d68d99fd021542a33a7bb84e4fd49ad5c14d08c
Siarhei Stamal <sergei@stml.33mail.com>
2015-05-11 00:11:20 +0300

    added js/app.js and js/controllers.js

---
+++ js/app.js
@@ -0,0 +1,27 @@
+/**
+ * Created by stml on 5/10/15.
+ */
+'use strict';
+
+/* App Module */
+
+var dietApp = angular.module('notesApp', [
+
+    'dietControllers'
+]);
+
+//phonecatApp.config(['$routeProvider',
+//    function($routeProvider) {
+//        $routeProvider.
+//            when('/phones', {
+//                templateUrl: 'partials/phone-list.html',
+//                controller: 'PhoneListCtrl'
+//            }).
+//            when('/phones/:phoneId', {
+//                templateUrl: 'partials/phone-detail.html',
+//                controller: 'PhoneDetailCtrl'
+//            }).
+//            otherwise({
+//                redirectTo: '/phones'
+//            });
+//    }]);
---
+++ js/controllers.js
@@ -0,0 +1,26 @@
+/**
+ * Created by stml on 5/10/15.
+ */
+'use strict';
+var setButton = angular.module('dietControllers', []);
+    setButton.controller('MainCtrl', [function() {
+    //    var self = this;
+    //}]);
+
+    //setButton.controller('answerController', [function() {
+        var self = this;
+        self.save = function(){
+            alert("asdasd");
+        };
+//            self.getTotal = function() {
+//                console.log('User clicked submit with ', self.user);
+//            };
+//         if(self.gender == "female"){
+//             console.log("123");
+//         BMR = 66 + [13.7 x вес (кг)] + [5 x рост (см)] - [6.76 x возраст (в годах)]
+//
+//         }else{
+//             console.log("456");
+//        BMR = 655 + [9.6 x вес (кг)] + [1.8 x рост (см)] - [4.7 x возраст (в годах)]
+//         }
+    }]);
